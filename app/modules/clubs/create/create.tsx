f39dbe0ae40d401b822b4d756805c718abfa3460
Dương Xuân Thắng <thang0912527403@gmail.com>
2026-04-12 20:22:32 +0700

    add create

--- app/modules/clubs/create/create.tsx
+++ app/modules/clubs/create/create.tsx
@@ -100,7 +100,6 @@ export default function CreateClubModule() {
             <Sidebar
                 currentPath="/clubs/create"
                 isOpen={isSidebarOpen}
-                onClose={toggleSidebar}
             />
 
             <HeaderBar
