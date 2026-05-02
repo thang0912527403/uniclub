591cd484c96c266190cf9ccfe2647d9265706282
ntKimaoo <nhukien0603@gmail.com>
2026-05-02 16:03:12 +0700

    fix sidebar translatation

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import type { Route } from "./+types/root";
 import "./app.css";
 import "~/modules/funds/utils/clubFundDetailAuthSync";
+import "~/utils/i18n";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
